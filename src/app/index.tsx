d297a380d4eda888e9fdd630c3a8800942e60573
Adrian Roach <adrianroach@ebongarde.com>
2018-07-21 19:21:51 -0400

    Fixed App not being used

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,6 @@
+import { App } from 'app/App/App';
 import 'app/index.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1 className="main-header">Hello, World!</h1>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
